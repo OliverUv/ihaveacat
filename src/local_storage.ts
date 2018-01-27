1cf79b17feee9e441e9db7ae5ae43b71864a265e
OliverUv <oliver.uvman@gmail.com>
2018-01-27 15:17:33 +0800

    Update local storage

--- src/local_storage.ts
+++ src/local_storage.ts
@@ -1,4 +1,4 @@
-export function save (state:object) {
+export function save(state:object) {
   try {
     localStorage.setItem('ihaveacat', JSON.stringify(state));
   } catch (e) {
@@ -6,11 +6,14 @@ export function save (state:object) {
   }
 }
 
-export function load() {
+export function load() : object|undefined {
   const game_state = localStorage.getItem('ihaveacat');
-  if (game_state) {
-    try {
-      return JSON.parse(game_state);
-    } catch (e) {}
+  if (!game_state) { return undefined; }
+  try {
+    return JSON.parse(game_state);
+  } catch (e) {
+    console.error(`local storage parse error`);
+    console.error(e);
+    return undefined;
   }
 }

152317078d997438e89b183948b96334e62ed73c
Necolo Chen <negko311@gmail.com>
2018-01-28 12:26:43 +0800

    finish pc_chat, npc_chat, toolbar, system_message

--- src/components/chat/npc_chat.tsx
+++ src/components/chat/npc_chat.tsx
@@ -1,16 +1,49 @@
 import * as React from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
 
+import {
+    Popover,
+    ListItem,
+} from 'react-onsenui';
+
+import { NodeType } from '../../game';
+import { TransKey } from '../../trans';
+
 interface NPCChatProps {
+    type:NodeType.pcimage | NodeType.pcsay;
+    content:TransKey;
+    npc:TransKey;
 }
 
 interface NPCChatState {
 }
 
 export class NPCChat extends React.Component<NPCChatProps, NPCChatState> {
+    public getTarget = () => {
+        return this.refs.avatar;
+    }
     public render() {
+        const props = this.props;
+
         return (
-            <p />
+            <ListItem modifier='nodivider' style={{marginTop: 16}}>
+                <section className='left' ref='avatar'>
+                    <img src={'http://placekitten.com/g/40/40'/* FIXME:*/} className='list-item_thumbnail' />
+                </section>
+
+                <Popover
+                    isOpen={true}
+                    isCancelable={false}
+                    getTarget={this.getTarget}
+                    animation='none'
+                    maskColor='rgba(0,0,0,0)'
+                    >
+
+                    <section>
+                        <FormattedHTMLMessage id={props.content} />
+                    </section>
+                </Popover>
+            </ListItem>
         );
     }
 }
--- src/components/chat/system_message.tsx
+++ src/components/chat/system_message.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
 
+import { ListItem } from 'react-onsenui';
+
+import { TransKey } from '../../trans';
+
 interface SystemMessageProps {
+    text:TransKey;
 }
 
 interface SystemMessageState {
@@ -10,7 +15,14 @@ interface SystemMessageState {
 export class SystemMessage extends React.Component<SystemMessageProps, SystemMessageState> {
     public render() {
         return (
-            <p />
+            <ListItem modifier='nodivider'>
+                <div style={{
+                    width: '100%',
+                    textAlign: 'center',
+                }}>
+                    <FormattedHTMLMessage id={this.props.text} />
+                </div>
+            </ListItem>
         );
     }
 }
